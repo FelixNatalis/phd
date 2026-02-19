335bf171e09101b7d2d7fbe6b3bd69c4b50d52dc
Nataliia Zubareva <60274420+HowToCodeWithPaws@users.noreply.github.com>
2026-02-19 17:02:56 +0100

    work

--- app_improved.R
+++ app_improved.R
@@ -16,22 +16,39 @@ epsilon = 1e-6
 # Kernels
 
 # SE kernel function
-squared_exponential_kernel <- function(x1, x2, lambda, sigma_2) {
+squared_exponential_kernel <- function(x1, x2, length_scale = 1, variance = 1, roughness = 2.5, period = 1) {
   outer(x1, x2, function(a, b)
-    (sigma_2 * exp(-(a - b)^2 / (2 * lambda^2)))
+    (variance * exp(-(a - b)^2 / (2 * length_scale^2)))
   )
 }
 
 # Linear kernel function
-linear_kernel <- function(x1, x2, lambda, sigma_2) {
+linear_kernel <- function(x1, x2, length_scale = 1, variance = 1, roughness = 2.5, period = 1) {
   outer(x1, x2, function(a, b)
-    (sigma_2 * a * b)
+    (variance * a * b)
   )
 }
 
+# Matérn kernel function
+matern_kernel <- function(x1, x2, length_scale = 1, variance = 1, roughness = 1.5, period = 1) {
+  outer(x1, x2, function(a, b){
+    length_scale = 1
+    variance = 1
+    roughness = 1.5
+    distance <- (a - b)^2
+
+    term <- sqrt(2 * roughness) * ((a - b)^2 + 1e-5) / length_scale^2
+    matrix<- variance * 2^(1 - roughness) / gamma(roughness) * term^roughness * besselK(term, roughness)
+    matrix[distance == 0] <- 1
+    return (matrix)
+  }
+  )
+}
+
+
 kernels <- hash(
   "Squared Exponential" = squared_exponential_kernel
-  #,"Matérn" = 2, 
+  ,"Matérn" = matern_kernel
   ,"Linear" = linear_kernel
   #,"Periodic" = 4
   ) 
@@ -45,9 +62,9 @@ kernels <- hash(
 # )
 
 # Drawing GP
-simulate_gp <- function(x, kernel, lambda, sigma_k, sigma_noise = 1e-3, mean_fun = function(x) 0) {
+simulate_gp <- function(x, kernel, length_scale, variance, sigma_noise = 1e-3, mean_fun = function(x) 0) {
   
-  K <- kernel(x, x, lambda, sigma_k)
+  K <- kernel(x, x, length_scale, variance)
   L <- chol(K + epsilon * diag(length(x)))
   
   m <- mean_fun(x)
@@ -73,21 +90,21 @@ ui <- fluidPage(
       ),
       tags$hr(),
       
-      tags$h4("Hyperparameters for λ"),
+      tags$h4("Hyperparameters for length scale"),
       sliderInput("ig_alpha", "Inverse-Gamma alpha:", 1, 15, 1),
       sliderInput("ig_beta", "Inverse-Gamma beta:", 1, 15, 1),
       #sliderInput("ig_mu", "Inverse-Gaussian mean:", 1, 15, 1),
-      #sliderInput("ig_lambda", "Inverse-Gaussian shape:", 1, 15, 1),
-      checkboxInput("lambda_mle", "Use MLE", FALSE),
-      actionButton("draw_lambda", "Draw New Lambda"),
+      #sliderInput("ig_length_scale", "Inverse-Gaussian shape:", 1, 15, 1),
+      checkboxInput("length_scale_mle", "Use MLE", FALSE),
+      actionButton("draw_length_scale", "Draw New Length Scale"),
       tags$hr(),
       
-      tags$h4("Hyperparameters for σ^2"),
+      tags$h4("Hyperparameters for variance"),
       sliderInput("ht_mu", "Half-t mean:", 0, 15, 0),
       sliderInput("ht_df", "Half-t degrees of freedom:", 1, 5, 4),
       sliderInput("ht_scale", "Half-t scale:", 1, 15, 1),
-      checkboxInput("sigma_2_mle", "Use MLE", FALSE),
-      actionButton("draw_sigma_2", "Draw New Sigma^2"),
+      checkboxInput("variance_mle", "Use MLE", FALSE),
+      actionButton("draw_variance", "Draw New Variance"),
       tags$hr(),
 
       #tags$h4("Other parameters"),
@@ -123,12 +140,12 @@ server <- function(input, output) {
   
 
   #Inv-Gamma
-  lambda_draw <- eventReactive(input$draw_lambda, { 
+  length_scale_draw <- eventReactive(input$draw_length_scale, { 
     seed_val <- digest(list(input$ig_alpha, input$ig_beta), algo="xxhash32", serialize=TRUE) |> 
       substr(1,7) |> strtoi(base=16)
     set.seed(seed_val)
     
-    if (isTRUE(input$lambda_mle)) {
+    if (isTRUE(input$length_scale_mle)) {
       return(input$ig_beta / (input$ig_alpha + 1))   # MLE of InvGamma(α,β)
     } else {
       return(1 / rgamma(1, shape = input$ig_alpha, rate = input$ig_beta))
@@ -136,25 +153,25 @@ server <- function(input, output) {
   })
   
   # Inv-Gaussian
-  #lambda_draw_gauss <- eventReactive(input$draw_lambda, { 
-  #  seed_val <- digest(list(input$ig_mu, input$ig_lambda), algo="xxhash32", serialize=TRUE) |> 
+  #length_scale_draw_gauss <- eventReactive(input$draw_length_scale, { 
+  #  seed_val <- digest(list(input$ig_mu, input$ig_length_scale), algo="xxhash32", serialize=TRUE) |> 
   #    substr(1,7) |> strtoi(base=16)
   #  set.seed(seed_val)
   #  
-  #  if (isTRUE(input$lambda_mle)) {
-  #    return(input$ig_mu*(sqrt(1 + (9*input$ig_mu)/(4*input$ig_lambda))-(3*input$ig_mu)/(2*input$ig_lambda)))   # MLE of InvGaussian?
+  #  if (isTRUE(input$length_scale_mle)) {
+  #    return(input$ig_mu*(sqrt(1 + (9*input$ig_mu)/(4*input$ig_length_scale))-(3*input$ig_mu)/(2*input$ig_length_scale)))   # MLE of InvGaussian?
   #  } else {
-  #    return(rinvgauss(1, mean=input$ig_mu, shape=input$ig_lambda))
+  #    return(rinvgauss(1, mean=input$ig_mu, shape=input$ig_length_scale))
   #  }
   #})
   
   
-  sigma_2_draw <- eventReactive(input$draw_sigma_2, {
+  variance_draw <- eventReactive(input$draw_variance, {
     seed_val <- digest(list(input$ht_mu, input$ht_df, input$ht_scale), algo="xxhash32", serialize=TRUE) |> 
       substr(1,7) |> strtoi(base=16)
     set.seed(seed_val)
     
-    if (isTRUE(input$sigma_2_mle)) {
+    if (isTRUE(input$variance_mle)) {
       return(input$ht_mu)    # half-t MLE occurs at lower bound = μ
     } else {
       return(input$ht_mu + input$ht_scale * varhalfT(runif(1), n=input$ht_df))
@@ -163,11 +180,11 @@ server <- function(input, output) {
   
   
   gp_funcs <- reactive({
-    req(kernel_choice(), lambda_draw(), sigma_2_draw())
+    req(kernel_choice(), length_scale_draw(), variance_draw())
     
     x_orig <- seq(x_min, x_max, length.out = n_points)
     funcs <- replicate(input$nfunc,
-                       simulate_gp(x_orig, kernel_choice(), lambda_draw(), sigma_2_draw()))#, mean_fun = function(x) 10 + 5 * x_orig) ))
+                       simulate_gp(x_orig, kernel_choice(), length_scale_draw(), variance_draw()))#, mean_fun = function(x) 10 + 5 * x_orig) ))
     
     list(x_orig = x_orig, funcs = funcs)
   })
@@ -177,11 +194,11 @@ server <- function(input, output) {
   last_pool   <- reactiveVal(NULL)
   
   gp_pool <- eventReactive(input$draw_gp, {
-    req(kernel_choice(), lambda_draw(), sigma_2_draw())
+    req(kernel_choice(), length_scale_draw(), variance_draw())
     
     kernel <- kernel_choice()
-    lam <- lambda_draw()
-    sig <- sigma_2_draw()
+    len <- length_scale_draw()
+    var <- variance_draw()
     old_params <- last_params()
     old_pool   <- last_pool()
     
@@ -189,8 +206,8 @@ server <- function(input, output) {
     if (!is.null(old_params) &&
         is.list(old_params) &&
         identical(old_params$kernel_prev, kernel) &&
-        identical(signif(old_params$lambda,10), signif(lam,10)) &&
-        identical(signif(old_params$sigma_2,10),  signif(sig,10)) &&
+        identical(signif(old_params$length_scale,10), signif(len,10)) &&
+        identical(signif(old_params$variance,10),  signif(var,10)) &&
         !is.null(old_pool) &&
         is.list(old_pool)) {
       
@@ -200,13 +217,13 @@ server <- function(input, output) {
     x_orig <- seq(x_min, x_max, length.out = n_points)
     funcs <- replicate(
       n_functions,
-      simulate_gp(x_orig, kernel, lam, sig)#, mean_fun = function(x) 10 + 5 * x_orig)
+      simulate_gp(x_orig, kernel, len, var)#, mean_fun = function(x) 10 + 5 * x_orig)
 
     )
     
     new_pool <- list(x_orig = x_orig, funcs = funcs)
     
-    last_params(list(kernel_prev = kernel, lambda = lam, sigma_2 = sig))
+    last_params(list(kernel_prev = kernel, length_scale = len, variance = var))
     last_pool(new_pool)
     
     new_pool
@@ -236,9 +253,9 @@ server <- function(input, output) {
   
   ### plots
   
-  # Inverse-Gamma prior for lambda plot
+  # Inverse-Gamma prior for length_scale plot
   output$plot_ig <- renderPlot({
-    req(lambda_draw())
+    req(length_scale_draw())
     
     x <- seq(1e-6, 15, length.out = 400)
     alpha <- input$ig_alpha
@@ -249,11 +266,11 @@ server <- function(input, output) {
     
     ggplot(d, aes(x,y)) +
       geom_line(color="steelblue", linewidth=1) +
-      geom_point(aes(x=lambda_draw(), y=0), color="red", size=3) +
+      geom_point(aes(x=length_scale_draw(), y=0), color="red", size=3) +
       annotate("text",
                x = max(d$x) * 0.8,
                y = max(d$y) * 0.9,
-               label = paste0("λ = ", signif(lambda_draw(),3)),
+               label = paste0("λ = ", signif(length_scale_draw(),3)),
                color="red",
                size = 5,
                hjust = 0) +
@@ -262,22 +279,22 @@ server <- function(input, output) {
       theme_minimal(base_size=14)
   })
   
-  # Inverse-Gaussian prior for lambda plot
+  # Inverse-Gaussian prior for length_scale plot
   #output$plot_ig_gauss <- renderPlot({
-  #  req(lambda_draw_gauss())
+  #  req(length_scale_draw_gauss())
   #  
   #  x <- seq(1e-6, 50, length.out = 400)
   #  
-  #  dens <- dinvgauss(x, mean=input$ig_mu, shape=input$ig_lambda)
+  #  dens <- dinvgauss(x, mean=input$ig_mu, shape=input$ig_length_scale)
   #  d <- data.frame(x=x, y=dens)
   #  
   #  ggplot(d, aes(x,y)) +
   #    geom_line(color="steelblue", linewidth=1) +
-  #    geom_point(aes(x=lambda_draw_gauss(), y=0), color="red", size=3) +
+  #    geom_point(aes(x=length_scale_draw_gauss(), y=0), color="red", size=3) +
   #    annotate("text",
   #             x = max(d$x) * 0.8,
   #             y = max(d$y) * 0.9,
-  #             label = paste0("λ = ", signif(lambda_draw_gauss(),3)),
+  #             label = paste0("λ = ", signif(length_scale_draw_gauss(),3)),
   #             color="red",
   #             size = 5,
   #             hjust = 0) +
@@ -286,9 +303,9 @@ server <- function(input, output) {
   #    theme_minimal(base_size=14)
   #})
   
-  # Half-t prior for sigma plot
+  # Half-t prior for variance plot
   output$plot_ht <- renderPlot({
-    req(sigma_2_draw())
+    req(variance_draw())
     
     x <- seq(0, 15, length.out = 400)
     df <- input$ht_df
@@ -301,11 +318,11 @@ server <- function(input, output) {
     
     ggplot(d, aes(x,y)) +
       geom_line(color="darkgreen", linewidth=1) +
-      geom_point(aes(x=sigma_2_draw(), y=0), color="red", size=3) +
+      geom_point(aes(x=variance_draw(), y=0), color="red", size=3) +
       annotate("text",
                x = max(d$x) * 0.8,
                y = max(d$y) * 0.9,
-               label = paste0("σ^2 = ", signif(sigma_2_draw(),3)),
+               label = paste0("σ^2 = ", signif(variance_draw(),3)),
                color="red",
                size = 5,
                hjust = 0) +
@@ -316,14 +333,14 @@ server <- function(input, output) {
   
  # Kernel based on distance plot
   output$kernelPlot <- renderPlot({
-    req(kernel_choice(), lambda_draw(), sigma_2_draw())
+    req(kernel_choice(), length_scale_draw(), variance_draw())
     
     dist <- seq(-3, 3, length.out = 300)
     x_o <- rep(0, length(dist))
-    lambda <- lambda_draw()
-    sigma_2  <- sigma_2_draw()
+    length_scale <- length_scale_draw()
+    variance  <- variance_draw()
     
-    k <- kernel_choice()(dist, x_o, lambda, sigma_2)
+    k <- kernel_choice()(dist, x_o, length_scale, variance)
     
     ggplot(data.frame(dist=dist, k=k), aes(dist,k)) +
       geom_line(color="purple", linewidth=1.2) +
