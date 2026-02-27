88de6b383f7765c92575e3599919d762b0cb6684
Nataliia Zubareva <60274420+HowToCodeWithPaws@users.noreply.github.com>
2026-02-27 17:00:32 +0100

    refactor

--- prior_predictive_app.R
+++ prior_predictive_app.R
@@ -10,6 +10,12 @@ library(bslib)
 library(gtools)
 library(shinyjs)
 
+#-------------------------------------------------------------------------------
+################################################################################
+# GLOBAL CONSTANTS
+################################################################################
+#-------------------------------------------------------------------------------
+
 # defaults parameters for GP prior predictive draws
 n_functions = 10
 x_min = -10
@@ -17,7 +23,13 @@ x_max = 10
 n_points = 200
 epsilon = 1e-6
 
-# Kernels
+#-------------------------------------------------------------------------------
+################################################################################
+# LIBRARY
+################################################################################
+#-------------------------------------------------------------------------------
+
+## Kernels
 
 # SE kernel function
 squared_exponential_kernel <- function(x1, x2, variance, length_scale) {
@@ -95,6 +107,7 @@ kernel_wrapper <- function(kernel_label, x1, x2, params){
   }
 }
 
+#-------------------------------------------------------------------------------
 # Drawing GP
 simulate_gp <- function(x, kernel_label, kernel_params, sigma_noise = 1e-3, mean_fun = function(x) 0) {
   K <- kernel_wrapper(kernel_label, x, x, params = kernel_params) 
@@ -107,8 +120,9 @@ simulate_gp <- function(x, kernel_label, kernel_params, sigma_noise = 1e-3, mean
   
   drop(f + eps)
 }
+#-------------------------------------------------------------------------------
+## Distributions
 
-# Distributions
 # inverse gamma
 inverse_gamma <- function(alpha, beta, x = NULL){
   if(invalid(x)){
@@ -129,11 +143,25 @@ half_t <- function(df, scale, x = NULL){
   }
 }
 
-
-## UI
+#-------------------------------------------------------------------------------
+################################################################################
+# UI
+################################################################################
+#-------------------------------------------------------------------------------
 
 ui <- page_fillable(
-  tags$style(type='text/css', " .selectize-input { font-size:80%; line-height: 16px;} .selectize-dropdown { font-size:80%; line-height: 16px; } .control-label { font-size:80%;}
+  tags$style(type='text/css', 
+  ".selectize-input { 
+    font-size:80%; 
+    line-height: 16px;
+  } 
+  .selectize-dropdown { 
+    font-size:80%; 
+    line-height: 16px; 
+  } 
+  .control-label { 
+    font-size:80%;
+  }
   .card-header {
     padding: 4px;
     font-size:80%;
@@ -142,9 +170,10 @@ ui <- page_fillable(
     padding: 2px 6px;
     font-size: 80%;
   }
-               .card-body {
+  .card-body {
     display: inline-block;
   }"),
+  
   useShinyjs(),
   layout_columns(
     card( "Parameters",
@@ -187,14 +216,21 @@ ui <- page_fillable(
         card(plotOutput("kernelPlot", height = "250px"))
       ),
       card(plotOutput("gpPlot", height = "600px"))
+      
     ),
  col_widths = c(2, 10) 
   )
 )
 
+#-------------------------------------------------------------------------------
+################################################################################
+# SERVER
+################################################################################
+#-------------------------------------------------------------------------------
 
 server <- function(input, output) {
   
+  ## Observable variables
   last_params   <- reactiveVal(NULL)
   last_pool     <- reactiveVal(NULL)
   variance      <- reactiveVal(NULL)
@@ -202,45 +238,16 @@ server <- function(input, output) {
   roughness     <- reactiveVal(NULL)
   period        <- reactiveVal(NULL)
 
-  shinyjs::disable("draw_kernel")
-  
   rv <- reactiveValues(
     var = FALSE,
     ls  = FALSE,
     per = FALSE
   )
   
-  #Inv-Gamma
-  length_scale_draw <- eventReactive(input$draw_length_scale, { 
-    seed_val <- digest(list(input$ls_ig_alpha, input$ls_ig_beta), algo="xxhash32", serialize=TRUE) |> 
-      substr(1,7) |> strtoi(base=16)
-    set.seed(seed_val)
-    
-    rv$ls <- TRUE
-    ls <- inverse_gamma(alpha = input$ls_ig_alpha, beta = input$ls_ig_beta)
-    length_scale(ls)
-  })
+  #-------------------------------------------------------------------------------
+  ## Dynamic ui elements 
   
-  #Inv-Gamma
-  period_draw <- eventReactive(input$draw_period, { 
-    seed_val <- digest(list(input$per_ig_alpha, input$per_ig_beta), algo="xxhash32", serialize=TRUE) |> 
-      substr(1,7) |> strtoi(base=16)
-    set.seed(seed_val)
-    
-    rv$per <- TRUE
-    per <- inverse_gamma(alpha = input$per_ig_alpha, beta = input$per_ig_beta)
-    period(per)
-  })
-  
-  variance_draw <- eventReactive(input$draw_variance, {
-    seed_val <- digest(list(input$ht_df, input$ht_scale), algo="xxhash32", serialize=TRUE) |> 
-      substr(1,7) |> strtoi(base=16)
-    set.seed(seed_val)
-    
-    rv$var <- TRUE
-    var <- half_t(df = input$ht_df, scale = input$ht_scale)
-    variance(var) 
-  })
+  shinyjs::disable("draw_kernel")
   
   observe({
     if (input$kernel_label == "Linear" && rv$var) {
@@ -255,7 +262,7 @@ server <- function(input, output) {
       shinyjs::enable("draw_kernel")
       
     } else if (input$kernel_label == "Matérn" &&
-              rv$var && rv$ls) {
+               rv$var && rv$ls) {
       shinyjs::enable("draw_kernel")
       
     } else {
@@ -263,78 +270,6 @@ server <- function(input, output) {
     }
   })
   
-  gp_pool <- eventReactive(input$draw_gp, {
-    
-    kernel <- input$kernel_label
-    len <- length_scale()
-    var <- variance()
-    per <- period()
-    ro <- input$nu
-    old_params <- last_params()
-    old_pool   <- last_pool()
-    
-    # reuse pool when parameters unchanged
-    if (!is.null(old_params) &&
-        is.list(old_params) &&
-        identical(old_params$kernel_prev, kernel) &&
-        identical(signif(old_params$length_scale,10), signif(len,10)) &&
-        identical(signif(old_params$variance,10),  signif(var,10)) &&
-        identical(signif(old_params$period,10), signif(per,10)) &&
-        identical(signif(old_params$roughness,10), signif(ro,10)) &&
-        !is.null(old_pool) &&
-        is.list(old_pool)) {
-      
-      return(old_pool)
-    }
-    
-    x_orig <- seq(x_min, x_max, length.out = n_points)
-    
-    kernel_params <- hash(
-      "variance" = var
-      ,"length_scale" = len
-      ,"period" = per
-      ,"roughness" = ro
-    )
-    
-    funcs <- replicate(
-      n_functions,
-      simulate_gp(x_orig, input$kernel_label, kernel_params)#, mean_fun = function(x) 10 + 5 * x_orig) !!!
-
-    )
-    
-    new_pool <- list(x_orig = x_orig, funcs = funcs)
-    
-    last_params(list(kernel_prev = kernel, length_scale = len, variance = var, period = per, roughness = ro))
-    last_pool(new_pool)
-    
-    new_pool
-  })
-  
-  # For storing and reproducing draws that were already computed
-  gp_data <- reactive({
-    req(gp_pool())
-    
-    idx <- 1:input$nfunc
-    idx <- idx[idx <= 100]   # safety
-    
-    x_new <- seq(x_min, x_max, length.out = n_points)
-    
-    funcs_interp <- apply(gp_pool()$funcs[, idx, drop=FALSE], 2, function(f) {
-      approx(gp_pool()$x_orig, f, xout = x_new)$y
-    })
-    
-    data.frame(
-      x = rep(x_new, length(idx)),
-      f = as.vector(funcs_interp),
-      func = rep(idx, each = length(x_new))
-    )
-  })
-  
-  observeEvent(input$draw_kernel, {
-    enable("draw_gp")
-  })
-
-  # ui elements 
   
   output$dynamic_ls_choice <- renderUI({
     if (input$kernel_label != "Linear") {
@@ -380,12 +315,120 @@ server <- function(input, output) {
     }
   })
   
+  #-------------------------------------------------------------------------------
+  ## Reactive events
   
-  ### plots
+  # drawing length scale on button click
+  length_scale_draw <- eventReactive(input$draw_length_scale, { 
+    seed_val <- digest(list(input$ls_ig_alpha, input$ls_ig_beta), algo="xxhash32", serialize=TRUE) |> 
+      substr(1,7) |> strtoi(base=16)
+    set.seed(seed_val)
+    
+    rv$ls <- TRUE
+    ls <- inverse_gamma(alpha = input$ls_ig_alpha, beta = input$ls_ig_beta)
+    length_scale(ls)
+  })
+  
+  # drawing period on button click
+  period_draw <- eventReactive(input$draw_period, { 
+    seed_val <- digest(list(input$per_ig_alpha, input$per_ig_beta), algo="xxhash32", serialize=TRUE) |> 
+      substr(1,7) |> strtoi(base=16)
+    set.seed(seed_val)
+    
+    rv$per <- TRUE
+    per <- inverse_gamma(alpha = input$per_ig_alpha, beta = input$per_ig_beta)
+    period(per)
+  })
+  
+  # drawing variance on button click
+  variance_draw <- eventReactive(input$draw_variance, {
+    seed_val <- digest(list(input$ht_df, input$ht_scale), algo="xxhash32", serialize=TRUE) |> 
+      substr(1,7) |> strtoi(base=16)
+    set.seed(seed_val)
+    
+    rv$var <- TRUE
+    var <- half_t(df = input$ht_df, scale = input$ht_scale)
+    variance(var) 
+  })
+  
+  #-------------------------------------------------------------------------------
+  ## GP redraw logic
+  gp_pool <- eventReactive(input$draw_gp, {
+    
+    kernel <- input$kernel_label
+    len <- length_scale()
+    var <- variance()
+    per <- period()
+    ro <- input$nu
+    old_params <- last_params()
+    old_pool   <- last_pool()
+    
+    # reuse pool when parameters unchanged
+    if (!is.null(old_params) &&
+        is.list(old_params) &&
+        identical(old_params$kernel_prev, kernel) &&
+        identical(signif(old_params$length_scale,10), signif(len,10)) &&
+        identical(signif(old_params$variance,10),  signif(var,10)) &&
+        identical(signif(old_params$period,10), signif(per,10)) &&
+        identical(signif(old_params$roughness,10), signif(ro,10)) &&
+        !is.null(old_pool) &&
+        is.list(old_pool)) {
+      
+      return(old_pool)
+    }
+    
+    x_orig <- seq(x_min, x_max, length.out = n_points)
+    
+    kernel_params <- hash(
+      "variance" = var
+      ,"length_scale" = len
+      ,"period" = per
+      ,"roughness" = ro
+    )
+    
+    funcs <- replicate(
+      n_functions,
+      simulate_gp(x_orig, input$kernel_label, kernel_params)
+    )
+    
+    new_pool <- list(x_orig = x_orig, funcs = funcs)
+    
+    last_params(list(kernel_prev = kernel, length_scale = len, variance = var, period = per, roughness = ro))
+    last_pool(new_pool)
+    
+    new_pool
+  })
+  
+  # For storing and reproducing draws that were already computed
+  gp_data <- reactive({
+    req(gp_pool())
+    
+    idx <- 1:input$nfunc
+    idx <- idx[idx <= 100]   # safety
+    
+    x_new <- seq(x_min, x_max, length.out = n_points)
+    
+    funcs_interp <- apply(gp_pool()$funcs[, idx, drop=FALSE], 2, function(f) {
+      approx(gp_pool()$x_orig, f, xout = x_new)$y
+    })
+    
+    data.frame(
+      x = rep(x_new, length(idx)),
+      f = as.vector(funcs_interp),
+      func = rep(idx, each = length(x_new))
+    )
+  })
+  
+  observeEvent(input$draw_kernel, {
+    enable("draw_gp")
+  })
+  
+  #-------------------------------------------------------------------------------
+  ## plots
   
   # Inverse-Gamma prior for length_scale plot
   output$plot_ig <- renderPlot({
-    req(length_scale_draw())
+    observe(length_scale_draw())
     
     x_seq <- seq(1e-6, 15, length.out = 400)
     alpha <- input$ls_ig_alpha
@@ -394,26 +437,30 @@ server <- function(input, output) {
     dens <- inverse_gamma(alpha = alpha, beta = beta, x = x_seq) 
     d <- data.frame(x=x_seq, y=dens)
     
-    ggplot(d, aes(x,y)) +
+    plot <- ggplot(d, aes(x,y)) +
       geom_line(color="steelblue", linewidth=1) +
-      annotate("point", x = length_scale(), y = 0, colour = "red", size = 3) +
-      annotate("text",
-               x = Inf,
-               y = Inf,
-               hjust = 1.1,  
-               vjust = 1.5,
-               label = paste0("draw = ", signif(length_scale(),3)),
-               color="red",
-               size = 5,
-               hjust = 0) +
       labs(title="Inverse-Gamma prior for length scale",
            y="density", x="length scale") +
       theme_minimal(base_size=14)
+    
+    if(!invalid(length_scale())){
+      plot <- plot + annotate("point", x = length_scale(), y = 0, colour = "red", size = 3) +
+        annotate("text",
+                 x = Inf,
+                 y = Inf,
+                 hjust = 1.1,  
+                 vjust = 1.5,
+                 label = paste0("draw = ", signif(length_scale(),3)),
+                 color="red",
+                 size = 5)
+    }
+    
+    plot
   })
   
   # Half-t prior for variance plot
   output$plot_ht <- renderPlot({
-    req(variance_draw())
+    observe(variance_draw())
     
     x_seq <- seq(0, 15, length.out = 400)
     df <- input$ht_df
@@ -423,26 +470,31 @@ server <- function(input, output) {
 
     d <- data.frame(x=x_seq, y=dens)
     
-    ggplot(d, aes(x,y)) +
+    plot <- ggplot(d, aes(x,y)) +
       geom_line(color="darkgreen", linewidth=1) +
-      annotate("point", x = variance(), y = 0, colour = "red", size = 3) +
-      annotate("text",
-               x = Inf,
-               y = Inf,
-               hjust = 1.1,  
-               vjust = 1.5,
-               label = paste0("draw = ", signif(variance(),3)),
-               color="red",
-               size = 5,
-               hjust = 0) +
       labs(title="Half-t prior for variance",
            y="density", x="variance") +
       theme_minimal(base_size=14)
+    
+    if(!invalid(variance())){
+      plot <- plot + 
+        annotate("point", x = variance(), y = 0, colour = "red", size = 3) +
+        annotate("text",
+                 x = Inf,
+                 y = Inf,
+                 hjust = 1.1,  
+                 vjust = 1.5,
+                 label = paste0("draw = ", signif(variance(),3)),
+                 color="red",
+                 size = 5)
+    }
+    
+    plot
   })
   
   # Inverse-Gamma prior for period plot
   output$plot_ig_per <- renderPlot({
-    req(period_draw())
+    observe(period_draw())
 
     x_seq <- seq(1e-6, 15, length.out = 400)
     alpha <- input$per_ig_alpha
@@ -451,21 +503,26 @@ server <- function(input, output) {
     dens <- inverse_gamma(alpha = alpha, beta = beta, x = x_seq) 
     d <- data.frame(x=x_seq, y=dens)
 
-    ggplot(d, aes(x,y)) +
+    plot <- ggplot(d, aes(x,y)) +
       geom_line(color="steelblue", linewidth=1) +
-      annotate("point", x = period(), y = 0, colour = "red", size = 3) +
-      annotate("text",
-               x = Inf,
-               y = Inf,
-               hjust = 1.1,  
-               vjust = 1.5,
-               label = paste0("draw = ", signif(period(),3)),
-               color="red",
-               size = 5,
-               hjust = 0) +
       labs(title="Inverse-Gamma prior for period",
            y="density", x="period") +
       theme_minimal(base_size=14)
+    
+    if(!invalid(period())){
+      plot <- plot + 
+        annotate("point", x = period(), y = 0, colour = "red", size = 3) +
+        annotate("text",
+                 x = Inf,
+                 y = Inf,
+                 hjust = 1.1,  
+                 vjust = 1.5,
+                 label = paste0("draw = ", signif(period(),3)),
+                 color="red",
+                 size = 5)
+    }
+    
+    plot
  })
   
  # Kernel based on distance plot
@@ -493,7 +550,8 @@ server <- function(input, output) {
   
   # GP prior draws plot
   output$gpPlot <- renderPlot({
-    req(gp_data())
+    req(input$draw_gp)
+    observe(gp_data())#, period_draw(), length_scale_draw(), variance_draw())
     ggplot(gp_data(), aes(x=x, y=f, group=func, color=factor(func))) +
       geom_line(alpha=0.9, linewidth=1) +
       scale_color_discrete(guide="none") +
